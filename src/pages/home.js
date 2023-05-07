1086e22f0313c174756910dbdfde01afc6779835
Abduz Zami <69592754+AbduzZami@users.noreply.github.com>
2023-05-07 18:46:57 +0600

    asda

--- src/pages/home.js
+++ src/pages/home.js
@@ -91,6 +91,11 @@ function Home() {
           </div>
         </div>
         {/* carousel end */}
+
+        <div class="grid grid-rows-3 grid-flow-col gap-4">
+          <div class="row-span-3 ...">01</div>
+          <div class="row-span-2 ...">02</div>
+        </div>
       </div>
     </>
   );
